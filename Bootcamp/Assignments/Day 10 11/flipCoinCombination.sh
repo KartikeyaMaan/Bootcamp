8f7a1156df5fd6b82da621c2cda66c87b9f26842
KartikeyaMaan <kartikeya.maan1997@gmail.com>
2023-02-02 12:25:21 +0530

    updated flipCoinCOmbination problem with sorting

--- Bootcamp/Assignments/Day 10 11/flipCoinCombination.sh
+++ Bootcamp/Assignments/Day 10 11/flipCoinCombination.sh
@@ -1,4 +1,7 @@
 printf "\n ~~Singlet Count~~ \n\n"
+
+declare -A map
+
 headCount=0
 tailCount=0
 totalCount=0
@@ -17,12 +20,17 @@ done
 
 echo "Number of times coin flipped : $totalCount"
 
-declare -A singlet=([Heads]=$headCount [Tails]=$tailCount)
+declare -A singlet=([Head]=$headCount [Tail]=$tailCount)
+
+for key in ${!singlet[@]}
+do
+	map+=([${singlet[$key]}]=$key)
+done
 
 printf "\nSinglet dictionary :- \n\n"
 for key in ${!singlet[@]}
 do
-	echo "$key -> ${singlet[$key]}"
+        echo "$key -> ${singlet[$key]}"
 done
 printf "\n"
 hp=`echo $headCount $totalCount | awk '{print ($1/$2)*100}'`
@@ -42,39 +50,45 @@ totalCount=0
 random=$((RANDOM%1000))
 while [ $totalCount -lt $random ]
 do
-	if [ $((RANDOM%2)) -eq 0 ]
+        if [ $((RANDOM%2)) -eq 0 ]
         then
                 firstFlip=0
         else
-		firstFlip=1
+                firstFlip=1
         fi
 
-	if [ $((RANDOM%2)) -eq 0 ]
+        if [ $((RANDOM%2)) -eq 0 ]
         then
                 secondFlip=0
         else
                 secondFlip=1
         fi
 
-	if [ $firstFlip -eq 0 -a $secondFlip -eq 0 ]
-	then
-		((HH++))
-	elif [ $firstFlip -eq 0 -a $secondFlip -eq 1 ]
-	then
-		((HT++))
-	elif [ $firstFlip -eq 1 -a $secondFlip -eq 0 ]
-	then
-		((TH++))
-	elif [ $firstFlip -eq 1 -a $secondFlip -eq 1 ]
-	then
-		((TT++))
-	fi
+        if [ $firstFlip -eq 0 -a $secondFlip -eq 0 ]
+        then
+                ((HH++))
+        elif [ $firstFlip -eq 0 -a $secondFlip -eq 1 ]
+        then
+                ((HT++))
+        elif [ $firstFlip -eq 1 -a $secondFlip -eq 0 ]
+        then
+                ((TH++))
+        elif [ $firstFlip -eq 1 -a $secondFlip -eq 1 ]
+        then
+                ((TT++))
+        fi
 ((totalCount++))
 done
 
 printf "Number of times coin flipped : $(($totalCount*2)) \n\n"
 
 declare -A doublet=([HH]=$HH [HT]=$HT [TH]=$TH [TT]=$TT)
+
+for key in ${!doublet[@]}
+do
+        map+=([${doublet[$key]}]=$key)
+done
+
 
 printf "Doublet dictionary :- \n\n"
 for key in ${!doublet[@]}
@@ -101,10 +115,10 @@ random=$((RANDOM%1000))
 while [ $count -lt $random ]
 do
         num=$(((RANDOM%8)+1))
-	case $num in
-		1)hhh=$(($hhh+1)) ;; 2)hht=$(($hht+1)) ;; 3)hth=$(($hth+1)) ;; 4)htt=$(($htt+1)) ;;
-		5)thh=$(($thh+1)) ;; 6)tht=$(($tht+1)) ;; 7)tth=$(($hhh+1)) ;; 8)ttt=$(($hhh+1)) ;;
-	esac
+        case $num in
+                1)hhh=$(($hhh+1)) ;; 2)hht=$(($hht+1)) ;; 3)hth=$(($hth+1)) ;; 4)htt=$(($htt+1)) ;;
+                5)thh=$(($thh+1)) ;; 6)tht=$(($tht+1)) ;; 7)tth=$(($hhh+1)) ;; 8)ttt=$(($hhh+1)) ;;
+        esac
 ((count++))
 done
 
@@ -112,11 +126,17 @@ printf "Number of times coin flipped : $(($count*3)) \n\n"
 
 declare -A triplet=([HHH]=$hhh [HHT]=$hht [HTH]=$hth [HTT]=$htt [THH]=$thh [THT]=$tht [TTH]=$tth [TTT]=$ttt)
 
+for key in ${!triplet[@]}
+do
+        map+=([${triplet[$key]}]=$key)
+done
+
+
 printf "Triplet Dictionary :- \n\n"
 
 for key in ${!triplet[@]}
 do
-	echo "$key  ->  ${triplet[$key]}"
+        echo "$key  ->  ${triplet[$key]}"
 done
 printf "\n"
 hhh=`echo $hhh $count | awk '{print ($1/$2)*100}'`
@@ -136,12 +156,60 @@ echo "Tail-Head-Head % = $thh %"
 echo "Tail-Head-Tail % = $tht %"
 echo "Tail-Tail-Head % = $tth %"
 echo "Tail-Tail-Tail % = $ttt %"
-printf "\n ~~~~~~~END~~~~~~~~ \n\n"
+printf "\n~~~~~~~END~~~~~~~~ \n\n\n"
 
 
 #~~~~~~~~~~~~~~~~~~~~~END~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+printf "~~~~~SORTING NOW~~~~~~ \n\n"
 
+printf "MapSet :- \n\n"
 
+for key in ${!map[@]}
+do
+	echo "$key -> ${map[$key]}"
+done
 
+declare -a arr
 
+for key in ${!map[@]}
+do
+        arr+=($key)
+done
+
+function printArray(){
+	for((i=0; i<${#arr[@]}; i++))
+	do
+        	printf "${arr[$i]}  "
+	done
+	printf "\n"
+}
+printf "\nArray :- "
+printArray
+
+#sort array
+
+size=${#arr[@]}
+for((i=0; i<size; i++))
+do
+	for((j=i; j<size; j++))
+	do
+		if [ ${arr[i]} -ge ${arr[j]} ]
+		then
+			temp=${arr[i]}
+			arr[$i]=${arr[j]}
+			arr[$j]=$temp
+		fi
+	done
+done
+printf "\nSorted Array :- "
+printArray
+
+#take out data from map
+
+printf "\n\nFinal Sorted Answer :- \n\n"
+for((i=0; i<size; i++))
+do
+	echo "${arr[$i]}  ->  ${map[${arr[$i]}]}"
+done
+printf "\n~~~~~~~~~~~THE END~~~~~~~~~~~~~~~\n"
